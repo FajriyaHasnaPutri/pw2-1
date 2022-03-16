cd860a93fd265667c18e1f49656448342a4b3d61
hasna <fajriyahsn2282@gmail.com>
2022-03-16 18:02:59 +0700

    ver3

--- tambah.php
+++ tambah.php
@@ -1,21 +1,23 @@
 <h1>Form Tambah Data</h1>
 
 <form method="POST">
-    NIM :<input type="text" name="nim" required><br>
+    NIM : <input type="text" name="nim" required><br>
     NAMA : <input type="text" name="nama"><br>
-    KELAS : <input type="text" name="kelas" ><br>
+    KELAS : <input type="text" name="kelas"><br>
     <input type="submit" value="Simpan">
 </form>
 
 <?php
-if(isset($POST['nim']) and !empty($POST['nim'])) {
+if(isset($_POST['nim']) and !empty($_POST['nim'])) {
     include('koneksi.php');
-    $sql = "insert into mahasiswa(nim, nama, kelas) values('".
-      $_POST['nim'] . "','" . $_POST['nama'] . "','" . $_POST['KELAS'] . "'";
+    $sql = "insert into mahasiswa(nim, nama, kelas) values('" .
+      $_POST['nim'] . "','" . $_POST['nama'] . "','" . $_POST['kelas'] . "')";
     $result = pg_affected_rows(pg_query($sql));
-    echo "data tersimpan $result baris";
-  if($result == 1) {
-      header('Location: index.php');
-  }
+    if($result == 1) {
+        echo '<script type="text/javascript">';
+        echo 'alert("Data telah tersimpan");';
+        echo 'window.location.href = "index.php";';
+        echo '</script>';
+    }
 }
 ?>
